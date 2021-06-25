c7f16a1cb3b6216c6d78ec135733948480e8f54c
John Lin <johnlin72499@gmail.com>
2021-06-25 15:37:13 -0400

    set proxy to true

--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('API_DOMAIN'),
+  proxy: true,
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '2131f5399fd4e0ab647b55774ed49880'),
