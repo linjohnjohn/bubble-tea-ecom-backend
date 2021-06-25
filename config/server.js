b412cbb8105196fd911b3b63cee5a575246a2aef
John Lin <johnlin72499@gmail.com>
2021-06-25 15:53:01 -0400

    use ap url and domain

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('API_DOMAIN'),
+  url: env('API_URL'),
   proxy: true,
   admin: {
     auth: {
