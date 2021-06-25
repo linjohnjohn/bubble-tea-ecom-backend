b4528a54c5353eafcc31482edd6fca171f165696
John Lin <johnlin72499@gmail.com>
2021-06-25 16:29:27 -0400

    test unsecure cookies

--- extensions/users-permissions/controllers/Auth.js
+++ extensions/users-permissions/controllers/Auth.js
@@ -133,9 +133,9 @@ module.exports = {
         });
         ctx.cookies.set("token", token, {
           httpOnly: true,
-          secure: process.env.NODE_ENV === "production" ? true : false,
+          // secure: process.env.NODE_ENV === "production" ? true : false,
           maxAge: 1000 * 60 * 60 * 24 * 14, // 14 Day Age
-          // domain: process.env.NODE_ENV === "development" ? "localhost" : process.env.API_DOMAIN,
+          domain: process.env.NODE_ENV === "development" ? "localhost" : process.env.API_DOMAIN,
         });
 
         ctx.send({
