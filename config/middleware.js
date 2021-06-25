ca79dc482d69b0313f604ff982ee6b22fab95eb7
John Lin <johnlin72499@gmail.com>
2021-06-25 10:44:53 -0400

    update cors config for deployment

--- config/middleware.js
+++ config/middleware.js
@@ -1,8 +1,16 @@
-module.exports = {
-  settings: {
-    cors: {
-      enabled: true,
-      origin: ['http://localhost:3000', 'http://localhost:1337'],
+module.exports = ({ env }) => {
+  let origin = ['http://localhost:3000', 'http://localhost:1337'];
+
+  if (env('NODE_ENV') === 'production') {
+    origin = env('ALLOW_ORIGIN').split(' ') || 'https://bubble-tea-ecommerce.herokuapp.com';
+  }
+
+  return {
+    settings: {
+      cors: {
+        enabled: true,
+        origin,
+      },
     },
-  },
+  }
 };
