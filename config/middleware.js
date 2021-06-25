cf3c64a7afcb8bfb3617f6004bc6417770bae8b3
John Lin <johnlin72499@gmail.com>
2021-06-25 10:47:02 -0400

    fix ALLOW_ORIGIN undefined issue

--- config/middleware.js
+++ config/middleware.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => {
   let origin = ['http://localhost:3000', 'http://localhost:1337'];
 
   if (env('NODE_ENV') === 'production') {
-    origin = env('ALLOW_ORIGIN').split(' ') || 'https://bubble-tea-ecommerce.herokuapp.com';
+    origin = env('ALLOW_ORIGIN', 'https://bubble-tea-ecommerce.herokuapp.com').split(' ');
   }
 
   return {
