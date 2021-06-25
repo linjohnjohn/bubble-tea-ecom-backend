328cb8a54aa3ff275083e48a8703af93e9c8d30f
John Lin <johnlin72499@gmail.com>
2021-06-25 10:14:12 -0400

    update settings for production db and domain

--- config/database.js
+++ config/database.js
@@ -1,15 +1,46 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+
+module.exports = ({ env }) => {
+  if (env('NODE_ENV' === 'production')) {
+    const parse = require('pg-connection-string').parse;
+    const config = parse(process.env.DATABASE_URL);
+
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password,
+            ssl: {
+              rejectUnauthorized: false,
+            },
+          },
+          options: {
+            ssl: true,
+          },
+        },
       },
-      options: {
-        useNullAsDefault: true,
+    }
+  }
+
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'sqlite',
+          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        },
+        options: {
+          useNullAsDefault: true,
+        },
       },
     },
-  },
-});
+  }
+};
--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('API_DOMAIN'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '2131f5399fd4e0ab647b55774ed49880'),
